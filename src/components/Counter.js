4a8733403d5dfd6f485c07aa5e9ffa014ad5a77e
Dror <drorp24@gmail.com>
2020-07-20 12:31:24 +0300

    redux (cont'd)

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { increment, decrement } from '../redux/all'
+import { increment, decrement } from '../redux/counter'
 
 const Counter = () => {
   const dispatch = useDispatch()
-  const state = useSelector((state) => state.counter)
+  const counter = useSelector(state => state.counter.value)
   return (
     <div>
       <h2>Counter</h2>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <h3>{`Counter value is ${state}`}</h3>
+      <h3>{`Counter value is ${counter}`}</h3>
       <Link to='/home'>Back Home</Link>
     </div>
   )
